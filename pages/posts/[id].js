dcf531958afe91372ed9c304536367463fbb1c44
Chris Hannaby <chris.hannaby@netlify.com>
2021-11-12 11:31:22 -0700

    add odb post

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function Post({ id }) {
+  const { t } = useTranslation("common");
+  return (
+    <div>
+      <h1>{t("h1")}</h1>
+      <p>page: {id}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { id: "foo" },
+      },
+    ],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ locale, params }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "footer"])),
+      id: params.id,
+    },
+  };
+}
