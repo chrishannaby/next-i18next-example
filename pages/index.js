a756c148324cf6282c768151cd94b0f2888dcce5
Chris Hannaby <chris.hannaby@netlify.com>
2021-12-01 16:48:55 -0700

    use getServerSideProps on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,39 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 const Homepage = () => {
-
-  const router = useRouter()
-  const { t } = useTranslation('common')
+  const router = useRouter();
+  const { t } = useTranslation("common");
 
   return (
     <>
       <main>
-        <Header heading={t('h1')} title={t('title')} />
+        <Header heading={t("h1")} title={t("title")} />
+        <p>This page uses getServerSideProps</p>
         <div>
-          <Link
-            href='/'
-            locale={router.locale === 'en' ? 'de' : 'en'}
-          >
-            <button>
-              {t('change-locale')}
-            </button>
+          <Link href="/" locale={router.locale === "en" ? "de" : "en"}>
+            <button>{t("change-locale")}</button>
           </Link>
-          <Link href='/second-page'>
-            <button
-              type='button'
-            >
-              {t('to-second-page')}
-            </button>
+          <Link href="/second-page">
+            <button type="button">{t("to-second-page")}</button>
           </Link>
         </div>
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export const getStaticProps = async ({ locale }) => ({
+export const getServerSideProps = async ({ locale }) => ({
   props: {
-    ...await serverSideTranslations(locale, ['common', 'footer']),
+    ...(await serverSideTranslations(locale, ["common", "footer"])),
   },
-})
+});
 
-export default Homepage
+export default Homepage;
