9e01e643556a646e5818e61e1afbd4d2eaf93a29
Chris Hannaby <chris.hannaby@netlify.com>
2021-12-01 18:20:27 -0700

    Revert "update config"
    This reverts commit 30500127f85dd7564a48a8438d2ac4c2f7f6d3b6.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,18 +1,6 @@
-const i18nextHttpBackend = require("i18next-http-backend/cjs");
-
 module.exports = {
-  backend: {
-    backends: process.browser ? [i18nextHttpBackend] : [],
-  },
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "de"],
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
   },
-  ns: ["common", "second-page", "footer", "client-page"],
-  partialBundledLanguages: true,
-  react: {
-    useSuspense: false,
-  },
-  serializeConfig: false,
-  use: process.browser ? [i18nextHttpBackend] : [],
-};
+}
