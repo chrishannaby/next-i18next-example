d53ffe3fa6043fe90c632092f122fa511fc5dcc8
Chris Hannaby <chris.hannaby@netlify.com>
2021-12-02 17:50:38 -0700

    test function

---
+++ netlify/functions/test.js
@@ -0,0 +1,22 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+exports.handler = async () => {
+  try {
+    const content = await fs.readFile(
+      path.join(__dirname, "public/locales/en/common.json"),
+      {
+        encoding: "utf-8",
+      }
+    );
+    return {
+      statusCode: 200,
+      body: content,
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e,
+    };
+  }
+};
